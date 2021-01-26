584e0c11178033ef239a5ff99696a2a47edd5800
S.Kornev <post.kornev@gmail.com>
2021-01-26 17:39:55 +0300

    Initial Commit

---
+++ HelloWordApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWordApp
+//
+//  Created by Сергей Корнев on 26.01.2021.
+//  Copyright © 2021 Sergey Kornev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
